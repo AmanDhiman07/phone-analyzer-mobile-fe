de345a9267bcc95b8ab9aef9bec1669c7a0eb5b0
amandhiiman <aman.dhiimann@gmail.com>
2026-02-18 13:18:31 +0530

    Remove unused pages and update the main index screen to display a welcome message. Refactor the first tab to simplify its content.

--- src/app/(tabs)/firstTab/index.tsx
+++ src/app/(tabs)/firstTab/index.tsx
@@ -5,19 +5,7 @@ import { router } from "expo-router";
 export default function FirstTab() {
   return (
     <View>
-      <AppText>First Tab</AppText>
-      <Button
-        title="Go to Second Tab"
-        onPress={() => router.push("/(tabs)/secondTab")}
-      />
-      <Button
-        title="Go to Third Tab"
-        onPress={() => router.push("/(tabs)/thirdTab")}
-      />
-      <Button
-        title="Go to First Page"
-        onPress={() => router.push("/(tabs)/firstTab/firstPage")}
-      />
+      <AppText>welcome</AppText>
     </View>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,11 @@
 import { Redirect } from "expo-router";
+import { AppText } from "@/components/AppText";
+import { View } from "react-native";
 
 export default function IndexScreen() {
-  return <Redirect href="/(tabs)/firstTab" />;
+  return (
+    <View className="justify-center flex-1 text-red-500 p-4">
+      <AppText center>welcome, Phone Analyzer</AppText>
+    </View>
+  );
 }
